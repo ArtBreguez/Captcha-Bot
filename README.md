db4e58734e7fdd99b72e840763658d6468886518
artbreguez <artbreguez@ucl.br>
2024-02-06 15:07:56 -0300

    app flask + img b64 concluido

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 dataset:
 
 https://www.kaggle.com/datasets/parsasam/captcha-dataset
+
+Run:
+
+waitress-serve --port=8080 wsgi:application
--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 import cv2
 import numpy as np
+import base64
+from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
+
+app = Flask(__name__)
 
 class CaptchaModel:
     def __init__(self, model_path, char_pool):
@@ -16,9 +20,11 @@ class CaptchaModel:
             decoded_strings.append(decoded_string)
         return decoded_strings
 
-    def predict_image(self, image_path):
-        imagem = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-        imagem = imagem / 255.0
+    def predict_image(self, base64_image):
+        image_data = base64.b64decode(base64_image)
+        nparr = np.frombuffer(image_data, np.uint8)
+        imagem = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+        imagem = imagem / 255.0  # Normalização
         imagem = cv2.resize(imagem, (150, 40))
         imagem = np.expand_dims(imagem, axis=-1)
 
@@ -28,13 +34,23 @@ class CaptchaModel:
 
         return resultado
 
-if __name__ == "__main__":
-    MODEL_PATH = os.getenv("MODEL_PATH", "model")
-    IMAGE_PATH = os.getenv("IMAGE_PATH", "dataset/archive/1a1SZ.jpg")
-    CHAR_POOL = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+MODEL_PATH = 'model'
+CHAR_POOL = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    modelo_captcha = CaptchaModel(MODEL_PATH, CHAR_POOL)
+modelo_captcha = CaptchaModel(MODEL_PATH, CHAR_POOL)
 
-    resultado = modelo_captcha.predict_image(IMAGE_PATH)
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    base64_image = data.get('image')
+    if base64_image:
+        try:
+            resultado = modelo_captcha.predict_image(base64_image)
+            return jsonify({'prediction': resultado}), 200
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+    else:
+        return jsonify({'error': 'Image not provided'}), 400
 
-    print("Resultado:", resultado)
+if __name__ == '__main__':
+    app.run(debug=False)
